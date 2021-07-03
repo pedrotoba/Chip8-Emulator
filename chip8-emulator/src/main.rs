d796e160bcd013ab8e65a7f5d2bf8cac2c26da08
Pedro Jesus Torres Barrilado <ptorres207@alumno.uned.es>
2021-07-03 20:58:40 +0200

    Changed the folders for games and test. Implemented the reading of roms and upload into memory

--- chip8-emulator/src/main.rs
+++ chip8-emulator/src/main.rs
@@ -1,4 +1,45 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
 
-fn main() {
-    println!("Hello, world!");
+// Memory definition
+/*struct Memory{
+    // The memory size is 4096 as seen in http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+    mem: [u8; 4096]
+}*/
+
+fn main() -> io::Result<()> {
+
+    // 1- 
+    // Open a game file and reads all the contents
+    let mut f = File::open("tests/test_opcode.ch8")?;
+    let mut romdata = Vec::<u8>::new();
+
+    f.read_to_end(&mut romdata)?;
+
+    // Print all the data
+    //print!("Test: {:?}",romdata);
+
+    
+   /* let mut memory = Memory{ 
+        mem: [0;4096]
+    };*/
+
+    // 2- 
+    // Initialize memory
+    let mut memory : [u8;4096] = [0;4096];
+
+    // Copy the rom data to ram from 0x200
+    let offset = 0x200;
+    for i in 0..romdata.len()
+    {
+        memory[offset+i as usize] = romdata[i as usize];
+    }
+
+    // Print all the memory
+    for x in 0..memory.len() {
+        print!("{} ", memory[x]);
+    }
+    
+    Ok(())
 }
